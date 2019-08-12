dc12def2e54243c85c07168a402fd7061653c890
Cameron Roman <camroman95@gmail.com>
2019-08-12 01:06:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,8 +3,14 @@ second_number = 5
 
 sum = first_number + second_number
 
-difference = first_number - second_number
+if first_number > second_number
+  difference = first_number - second_number
+else
+  difference = second_number - first_number
 
 product = first_number * second_number
 
-quotient = first_number / second_number
+if first_number > second_number
+  quotient = first_number / second_number
+else
+  quotient = second_number / first_number
